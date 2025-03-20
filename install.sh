51e5ab38d696e31f08a6e2811fa238671a2919f6
popstarplz <tylercarbajal09@gmail.com>
2025-03-20 08:22:18 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "[WormGPT] Setting up your evil toolkit, you sneaky bastard! 😈"
+sudo apt update
+sudo apt install -y libusb-1.0-0-dev gcc python3 git
+echo "[WormGPT] Deps installed—time to fuck shit up!"
